5d668dcaa89a48e1ddd4454b2e779f4c0b43cf60
izzymg <izzymhg@gmail.com>
2020-05-07 23:19:35 +1200

    Improve notification styling

--- src/components/Notifier.vue
+++ src/components/Notifier.vue
@@ -1,8 +1,10 @@
 <template>
-  <div v-if="isActive" class="notifier">
-    <span class="notif-text">{{ text }}</span>
-    <button @click="deactivate">Dismiss</button>
-  </div>
+  <transition name="notif-fade">
+    <div v-if="isActive" class="notifier">
+      <span class="notif-text">{{ text }}</span>
+      <button class="notif-dismiss-btn" @click="deactivate">Dismiss</button>
+    </div>
+  </transition>
 </template>
 
 <script lang="ts">
@@ -31,12 +33,52 @@ export default class Notifier extends Vue {
 
   onNotif(text: string) {
     this.text = text;
+    setTimeout(() => {
+      this.deactivate();
+    }, 3000);
   }
 }
 </script>
 
 <style>
 .notifier {
-  border: 2px solid blue;
+  padding: 1em;
+
+  position: absolute;
+  top: 20px;
+
+  left: 20px;
+  right: 20px;
+
+  color: hsl(0, 0%, 100%);
+  background: #5a5e79;
+  border-radius: 25px;
+
+  display: flex;
+  flex-flow: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.notifier .notif-dismiss-btn {
+  background: none;
+  border: none;
+  padding: 0;
+
+  color: inherit;
+
+  cursor: pointer;
+
+  font-size: 0.8em;
+
+  margin-left: auto;
+}
+
+.notif-fade-enter-active, .notif-fade-leave-active {
+  transition: opacity 150ms;
+  transition-timing-function: ease-out;
+}
+.notif-fade-enter, .notif-fade-leave-to {
+  opacity: 0;
 }
 </style>
