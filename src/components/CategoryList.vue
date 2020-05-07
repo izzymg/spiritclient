aebb3b8ff36ff38fe4d1cbe48987019334c92dfc
izzymg <izzymhg@gmail.com>
2020-05-07 18:07:28 +1200

    Improve category  links

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -9,7 +9,7 @@
 
     <div v-else-if="state.tag == 'loaded'" class="categories">      
       <div class="category" v-for="cat of cats" :key="cat.name">
-        <router-link :to="'/' + cat.name">
+        <router-link class="category-link" :to="'/' + cat.name">
           <span class="category-name">{{ cat.name }}</span>
         </router-link>
       </div>
@@ -49,7 +49,6 @@ export default class CategoryList extends Vue {
 .hello .categories .category {
   margin: 0 0 0.3em 0;
   border-bottom: 2px solid hsl(0, 0%, 80%);
-  padding: 0.5em;
 
   cursor: pointer;
 
@@ -61,4 +60,11 @@ export default class CategoryList extends Vue {
 .hello .categories .category:hover {
   border-bottom: 2px solid hsl(138, 38%, 68%);
 }
+
+.hello .categories .category-link {
+  display: block;
+  padding: 1em;
+  width: 100%;
+}
+
 </style>
