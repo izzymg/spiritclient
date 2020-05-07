8c2edb8ab3f8cdaf237844411f82a8839077b7f1
izzymg <izzymhg@gmail.com>
2020-05-07 13:29:17 +1200

    Add PostForm to thread for reply submissions

--- src/components/ThreadPosts.vue
+++ src/components/ThreadPosts.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="catview">
     <h2>Spirit</h2>
+
     <p v-if="state.tag == 'loading'">Loading...</p>
+
     <h1 v-else-if="state.tag == 'error'">Sorry! {{ state.error }}</h1>
+
     <div v-else-if="state.tag == 'loaded'">
       <h2>{{ category }}</h2>
       <h2>Thread no.{{ posts[0].num }}</h2>
+
+      <PostForm :category="category" :thread="thread"/>
+
       <div class="thread-post-wrap" v-for="post in posts" :key="post.num">
         <p>no.{{ post.num }}</p>
         <p>{{ parseDate(post.createdAt) }}</p>
@@ -21,7 +27,13 @@ import { Loading, Loaded, Errored, State } from "@/modules/state";
 import { getThread, Post } from "@/modules/repo";
 import { parseDate } from "@/modules/fmt";
 
-@Component
+import PostForm from "@/components/PostForm.vue";
+
+@Component({
+  components: {
+    PostForm,
+  },
+})
 export default class ThreadPosts extends Vue {
   @Prop({required: true, type: String})
   private category!: string;
