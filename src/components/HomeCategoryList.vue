0f289fe07b2c2d01bcc99505b3c0cd12fc58c18e
izzymg <izzymhg@gmail.com>
2020-05-08 21:36:45 +1200

    Pull AJAX logic into home view root

---
+++ src/components/HomeCategoryList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="home-category-list">
+    <div class="category" v-for="cat of cats" :key="cat.name">
+      <router-link class="category-link" :to="'/' + cat.name">
+        <span class="category-name">{{ cat.name }}</span>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { getCategories, Category } from "@/modules/repo";
+@Component
+export default class HomeCategoryList extends Vue {
+  @Prop({ required: true })
+  private cats!: Category;
+}
+</script>
+
+<style>
+.home-category-list {
+  display: flex;
+  flex-direction: column;
+  flex: 1 0 auto;
+  width: 60%;
+  margin: 0 auto;
+}
+
+.home-category-list .category {
+  margin: 0 0 0.3em 0;
+  border-bottom: 2px solid hsl(0, 0%, 80%);
+
+  cursor: pointer;
+
+  transition-property: border-bottom;
+  transition-duration: 150ms;
+  transition-timing-function: ease-in;
+}
+
+.home-category-list .category:hover {
+  border-bottom: 2px solid hsl(138, 38%, 68%);
+}
+
+.home-category-list .category .category-link {
+  display: block;
+  padding: 1em;
+  width: 100%;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,46 @@
 <template>
   <div class="home">
-    <CategoryList/>
+    <p>Modernized imageboard.</p>
+
+    <h2>Categories</h2>
+
+    <p v-if="state.tag == 'loading'">Loading...</p>
+    <p v-else-if="state.tag == 'error'">
+      Sorry, couldn't load the categories! {{ state.error }}
+    </p>
+
+    <div v-else-if="state.tag == 'loaded'" class="categories">      
+      <HomeCategoryList :cats="cats"/>
+    </div>
   </div>
 </template>
 
-<script>
-import CategoryList from "@/components/CategoryList.vue"
+<script lang="ts">
+import HomeCategoryList from "@/components/HomeCategoryList.vue"
 
-export default {
-  name: "Home",
+import { Vue, Component } from "vue-property-decorator";
+import { getCategories, Category } from "@/modules/repo";
+import { Loading, Loaded, Errored, State } from "@/modules/state";
+
+/**
+Home view, fetches all categories and render a list of them.
+*/
+@Component({
   components: {
-    CategoryList
+    HomeCategoryList,
+  },
+})
+export default class Home extends Vue {
+  private state: State = { tag: "loading" };
+  private cats: Category[] = [];
+  
+  async created() {
+    try {
+      this.cats = await getCategories();
+      this.state = { tag: "loaded" };
+    } catch(err) {
+      this.state = { tag: "error", error: err };
+    }
   }
 }
 </script>
