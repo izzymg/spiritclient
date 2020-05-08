f9a7310fa164325a4972fcc85ac39b52bab884d1
izzymg <izzymhg@gmail.com>
2020-05-08 21:55:08 +1200

    Pull AJAX logic into category view root

---
+++ src/components/CategoryThreadList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="cat-thread-list">
+    <div class="cat-thread-preview" v-for="thread in threads" :key="thread.num">
+      <RouterLink :to="{ name: 'Thread', params: { thread: thread.num } }">
+        <ThreadPost isPreview :post="thread"/>
+      </RouterLink>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { Post } from "@/modules/repo";
+
+import ThreadPost from "@/components/ThreadPost.vue";
+
+/**
+Lists a preview of all threads in a category. 
+*/
+@Component({
+  components: {
+    ThreadPost,
+  }
+})
+export default class CategoryThreadList extends Vue {
+  @Prop({required: true})
+  private threads!: Post[];
+}
+</script>
+
+<style>
+.cat-thread-list {
+  display: flex;
+  flex-flow: row wrap;
+  text-align: left;
+}
+
+.cat-thread-list .cat-thread-preview {
+  flex-basis: 15%;
+  margin: 0.5em;
+}
+
+</style>
--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,16 +1,57 @@
 <template>
   <div class="category">
-    <CategoryView :category="this.$route.params['category']"/>
+    <p v-if="state.tag == 'loading'">Loading...</p>
+
+    <p v-else-if="state.tag == 'error'">
+      Sorry, couldn't load the category! {{ state.error }}
+    </p>
+
+    <div v-else-if="state.tag == 'loaded'">
+      <h1>{{ catView.name }}</h1>
+      <PostForm :category="catView.name"/>
+      <Toolbar @refresh="loadThreads"/>
+
+      <CategoryThreadList
+        :threads="catView.threads"
+      />
+    </div>
   </div>
 </template>
 
-<script>
-import CategoryView from "@/components/CategoryView.vue"
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import CategoryThreadList from "@/components/CategoryThreadList.vue"
+import PostForm from "@/components/PostForm.vue";
+import Toolbar from "@/components/Toolbar.vue";
 
-export default {
-  name: "Category",
+import { getCatView, CatView } from "@/modules/repo";
+import { Loading, Loaded, Errored, State } from "@/modules/state";
+
+/**
+Category view, renders a list of threads on the category. 
+*/
+@Component({
   components: {
-    CategoryView
+    PostForm,
+    Toolbar,
+    CategoryThreadList,
+  },
+})
+export default class Category extends Vue {
+  private state: State = { tag: "loading" };
+  private catView: CatView | null = null;
+
+  created() {
+    this.loadThreads();
+  }
+
+  async loadThreads() {
+    try {
+      this.catView = await getCatView(this.$route.params["category"]);
+      this.state = { tag: "loaded" };
+    } catch(err) {
+      this.state = { tag: "error", error: err }
+    }
   }
 }
 </script>
