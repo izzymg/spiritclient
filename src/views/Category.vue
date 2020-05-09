c253d87cb3747d2677420ed83273c0cb1959a4a2
izzymg <izzymhg@gmail.com>
2020-05-10 08:24:52 +1200

    Fix undefined err if no threads on a category

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -58,9 +58,9 @@ export default class Category extends Vue {
   async loadCatView() {
     try {
       this.catView = await getCatView(this.$route.params["category"]);
-      this.catView.threads = this.catView.threads.reverse();
+      this.catView.threads = this.catView?.threads?.reverse();
       this.state = { tag: "loaded" };
-      this.meta.categoryThreads = this.catView.threads.length;
+      this.meta.categoryThreads = this.catView?.threads?.length;
     } catch(err) {
       this.state = { tag: "error", error: err }
     }
